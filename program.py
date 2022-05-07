9536cc45ef676918d01d4ab40a98ad75fa30e0f4
Михаил Лавриненко <it_web@bk.ru>
2022-05-07 13:49:49 +0600

    homework.py

--- program.py
+++ program.py
@@ -1,1 +1,18 @@
-print('Я домашка')
+class Training:
+    """Базовый класс тренировки."""
+
+    def __init__(self,
+                 action: int,
+                 duration: float,
+                 weight: float,
+                 LEN_STEP: float,
+                 M_IN_KM: float
+                 ) -> None:
+        self.LEN_STEP = 0.65
+        self.M_IN_KM = 1000
+        self.action = 42
+    def get_distance(self) -> float:
+        """Получить дистанцию в км."""
+        return self.action * self.LEN_STEP / self.M_IN_KM
+
+print(Training)
